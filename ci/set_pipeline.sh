0d3f0b0115b3db2eb22ae79c80b11bb25af8b159
Maregg <simonpierreb@gmail.com>
2018-10-19 13:59:44 -0400

    Update set_pipeline.sh
    Nouveau nom de pipeline

--- ci/set_pipeline.sh
+++ ci/set_pipeline.sh
@@ -3,6 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ALIAS=${1:-emerald-squad}
-PIPELINE_NAME=${2:-sonar-scanner}
+PIPELINE_NAME=${2:-sonar-scanner-dotnet}
 
 fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c $DIR/pipeline.yml --non-interactive 
